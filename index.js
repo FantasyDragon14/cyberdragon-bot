decc29fdc22fc63e999f59632f6214dfc4f8aadc
Ayla <fantasy.dragon4ever@outlook.de>
2024-09-16 21:17:41 +0200

    still trying to get revolt.js working

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-//import { Client } from 'revolt.js'
-//const { Client } = import('revolt.js')
-const { Revolt_Client } = require('revolt.js');
+import { Revolt_Client } from 'revolt.js';
+//const { Revolt_Client } = import('revolt.js');
+//const { Revolt_Client } = require('revolt.js');
 
 require('dotenv').config()
 //const { token } = require('./token_discord.txt');
 
 const prefix = "!"
 
-const revolt_rlient = new Client();
+let revolt_client = new Revolt_Client();
 
 
-revolt_rlient.on("ready", async () => {
-	console.info('[Revolt] logged in as ' + client.user.username);
-        revolt_rlient.api.patch("/users/@me", { status: { text: "Testing...", presence: "Focus" } }); //status, presence can be: "Online" | "Idle" | "Focus" | "Busy" | "Invisible" | null | undefined // null | undefined leave unchanged
+revolt_client.on("ready", async () => {
+	console.info('[Revolt] logged in as ' + revolt_client.user.username);
+        revolt_client.api.patch("/users/@me", { status: { text: "Testing...", presence: "Focus" } }); //status, presence can be: "Online" | "Idle" | "Focus" | "Busy" | "Invisible" | null | undefined // null | undefined leave unchanged
 });
 
-revolt_rlient.on("message", async (message) => {
+revolt_client.on("message", async (message) => {
         if (message.content === prefix + "ping") {
                 message.channel.sendMessage("Pong!");
         }
 });
 
-revolt_rlient.loginBot(process.env.REVOLT_TOKEN);
+revolt_client.loginBot(process.env.REVOLT_TOKEN);
 
