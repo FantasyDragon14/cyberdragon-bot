7069c40c86a71c453bc48860d7495feac945b973
Ayla <fantasy.dragon4ever@outlook.de>
2024-09-16 20:14:28 +0200

    testing...

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client} = require('revolt.js');
+//import { Client } from 'revolt.js'
+//const { Client } = import('revolt.js')
+const { Revolt_Client } = require('revolt.js');
+
 require('dotenv').config()
 //const { token } = require('./token_discord.txt');
 
 const prefix = "!"
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const revolt_rlient = new Client();
 
 
-
-client.on("ready", async () => {
-	console.info('logged in as ' + client.user.username);
-        client.api.patch("/users/@me", { status: { text: "Testing...", presence: "Focus" } }); //status, presence can be: "Online" | "Idle" | "Focus" | "Busy" | "Invisible" | null | undefined // null | undefined leave unchanged
+revolt_rlient.on("ready", async () => {
+	console.info('[Revolt] logged in as ' + client.user.username);
+        revolt_rlient.api.patch("/users/@me", { status: { text: "Testing...", presence: "Focus" } }); //status, presence can be: "Online" | "Idle" | "Focus" | "Busy" | "Invisible" | null | undefined // null | undefined leave unchanged
 });
 
-client.on("message", async (message) => {
+revolt_rlient.on("message", async (message) => {
         if (message.content === prefix + "ping") {
                 message.channel.sendMessage("Pong!");
         }
 });
 
-client.loginBot(process.env.REVOLT_TOKEN);
+revolt_rlient.loginBot(process.env.REVOLT_TOKEN);
 
