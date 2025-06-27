a9f3c2c51771e98305318e0e39737c59386b23ed
Ayla <me@fantasydragon.xyz>
2025-06-27 12:46:24 +0000

    finally fixed leave messages not working

--- Extensions/greet.py
+++ Extensions/greet.py
@@ -30,11 +30,13 @@ async def member_joined(event: hikari.MemberCreateEvent) -> None:
     guild:hikari.GatewayGuild = event.get_guild()
     guild_name = guild.name
     
-    logger.info(f"{event.member.mention} joined {guild_name}, sending message")
+    logger.info(f"{event.member.mention} joined {guild_name}, sending message.")
+    logger.debug(f'guild_flags: {event.member.guild_flags}')
+    logger.debug(f'user flags: {event.member.flags}')
 
     #TODO make this message customizable in settings, maybe even per server
     msg = f"{event.member.mention} welcome to {guild_name} ^w^"
-    if hikari.GuildMemberFlags.DID_REJOIN in event.member.flags:
+    if hikari.GuildMemberFlags.DID_REJOIN in event.member.guild_flags:
         msg = f"Welcome back {event.member.mention}!"
     logger.debug("sending " + msg)
     await event.app.rest.create_message(guild.system_channel_id, msg, user_mentions=True)
@@ -42,10 +44,11 @@ async def member_joined(event: hikari.MemberCreateEvent) -> None:
 
 @loader.listener(hikari.MemberDeleteEvent)
 async def member_left(event: hikari.MemberDeleteEvent)  -> None:
-    guild:hikari.GatewayGuild = await event.get_guild()
-    logger.info(f"{event.old_member.display_name} left {guild.name}")
+    guild:hikari.GatewayGuild = event.get_guild()
+    logger.info(f"{event.user.display_name} left {guild.name}")
+    logger.debug(f'user flags: {event.user.flags}')
     
     #TODO make this message customizable in settings, maybe even per server
-    msg = f"{event.old_member.mention} just left the Server..."
+    msg = f"{event.user.mention} left"
     await event.app.rest.create_message(guild.system_channel_id, msg)
     pass
